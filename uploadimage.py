ad46a67918ddbc913de8a5650aa8ad58efed3380
Chris Moran <christopher.moran@versent.com.au>
2017-06-04 02:42:21 +0000

    initial file uploader

---
+++ uploadimage.py
@@ -0,0 +1,31 @@
+# Take the image from the command line and upload to S3
+# When this is done, a lambda function will launch an
+# analysis run against the image.
+
+import sys
+import os
+import boto3
+
+# Pretty simple means of getting command args.
+# would be better to use --key value
+# but for now, it's OK
+#
+if len(sys.argv) == 4:
+	filename=sys.argv[1]
+	bucket=sys.argv[2]
+	camdir=sys.argv[3]
+	print 'File name: ', filename
+	print 'Bucket:    ', bucket
+	print 'Cam Dir:   ', camdir
+
+#Send the file to our bucket for processing
+#	
+	upload_name = camdir + '/' + filename
+	data = open(filename, 'rb')
+	s3 = boto3.resource('s3')
+	s3.Bucket(bucket).put_object(Key=filename, Body=data)
+	
+else:
+	print 'Usage: ', sys.argv[0], ' file_name bucket cam_id'
+		
+
