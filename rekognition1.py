c5f9837ffdf579926fa1c0d5b7848a24c105d4e6
Chris Moran <ChrisMoran64@users.noreply.github.com>
2017-06-28 14:03:45 +1000

    Update rekognition1.py

--- rekognition1.py
+++ rekognition1.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 import json
 import urllib
 import boto3
+import uuid
 
 print('Loading function')
 
@@ -16,23 +17,25 @@ rekognition = boto3.client('rekognition')
 def detect_faces(bucket, key):
     response = rekognition.detect_faces(Attributes=["ALL"],Image={"S3Object": {"Bucket": bucket, "Name": key}})
     return response
+    
+def save_reaction(key, emotion, confidence):
+    db = boto3.resource('dynamodb').Table('Sentiments')
+#    entry_id = uuid.uuid4()
+    data =[ {'Emotion': str(emotion), 'Confidence': Decimal(str(confidence)) } ]
+    db.put_item(Item={ 'EntryId': str(uuid.uuid4()), 'ImageId': key, 'Data': data })
 
 
+# For future use
 def detect_labels(bucket, key):
     response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
 
-    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
-    # Note: role used for executing this Lambda function should have write access to the table.
-    #table = boto3.resource('dynamodb').Table('MyTable')
-    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
-    #table.put_item(Item={'PK': key, 'Labels': labels})
     return response
 
-
+# For future use
 def index_faces(bucket, key):
     # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
     #rekognition.create_collection(CollectionId='BLUEPRINT_COLLECTION')
-    response = rekognition.index_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}}, CollectionId="BLUEPRINT_COLLECTION")
+    response = rekognition.index_faces(Attributes=["ALL"], Image={"S3Object": {"Bucket": bucket, "Name": key}})
     return response
 
 
@@ -49,19 +52,16 @@ def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
     try:
-        # Calls rekognition DetectFaces API to detect faces in S3 object
+        print('Bucket', bucket, ' Key', key)
+        # Calls rekognition DetectFaces API to detect faces in S3 object.
         response = detect_faces(bucket, key)
 
-        # Calls rekognition DetectLabels API to detect labels in S3 object
-        #response = detect_labels(bucket, key)
+        # Print response to console and commit to database.
+        for em in response["FaceDetails"]:
+            for res in em["Emotions"]:
+                print("Emotion: ", res["Type"], "Confidence: ", res["Confidence"])
+                save_reaction(key, res["Type"], res["Confidence"])
 
-        # Calls rekognition IndexFaces API to detect faces in S3 object and index faces into specified collection
-        #response = index_faces(bucket, key)
-
-        # Print response to console.
-        print(response["FaceDetails"][0]["Emotions"][0]["Type"], response["FaceDetails"][0]["Emotions"][0]["Confidence"])
-        print(response["FaceDetails"][0]["Emotions"][1]["Type"], response["FaceDetails"][0]["Emotions"][1]["Confidence"])
-        print(response["FaceDetails"][0]["Emotions"][2]["Type"], response["FaceDetails"][0]["Emotions"][2]["Confidence"])
 
         return response
     except Exception as e:
