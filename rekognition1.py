60491afea792b7fac96b228ba5607c7c9e3c1ed8
Chris Moran <ChrisMoran64@users.noreply.github.com>
2017-07-03 21:05:56 +1000

    Lock the ImageId since it is a partition key

--- rekognition1.py
+++ rekognition1.py
@@ -20,18 +20,22 @@ def detect_faces(bucket, key):
     
 def save_reaction(key, emotion, confidence):
     db = boto3.resource('dynamodb').Table('Sentiments')
-#    entry_id = uuid.uuid4()
     data =[ {'Emotion': str(emotion), 'Confidence': Decimal(str(confidence)) } ]
-    db.put_item(Item={ 'EntryId': str(uuid.uuid4()), 'ImageId': key, 'Data': data })
+#    db.put_item(Item={ 'EntryId': str(uuid.uuid4()), 'ImageId': key, 'Data': data })
+    db.put_item(Item={ 'EntryId': '1', 'ImageId': key, 'Data': data })
 
 
-# For future use
 def detect_labels(bucket, key):
     response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
 
+    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
+    # Note: role used for executing this Lambda function should have write access to the table.
+    #table = boto3.resource('dynamodb').Table('MyTable')
+    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
+    #table.put_item(Item={'PK': key, 'Labels': labels})
     return response
 
-# For future use
+
 def index_faces(bucket, key):
     # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
     #rekognition.create_collection(CollectionId='BLUEPRINT_COLLECTION')
@@ -56,23 +60,16 @@ def lambda_handler(event, context):
         # Calls rekognition DetectFaces API to detect faces in S3 object.
         response = detect_faces(bucket, key)
 
-        # Print response to console.
-        #print(response["FaceDetails"][0]["Emotions"][0]["Type"], response["FaceDetails"][0]["Emotions"][0]["Confidence"])
-        #print(response["FaceDetails"][0]["Emotions"][1]["Type"], response["FaceDetails"][0]["Emotions"][1]["Confidence"])
-        #print(response["FaceDetails"][0]["Emotions"][2]["Type"], response["FaceDetails"][0]["Emotions"][2]["Confidence"])
-
         # Print response to console and commit to database.
         for em in response["FaceDetails"]:
             for res in em["Emotions"]:
                 print("Emotion: ", res["Type"], "Confidence: ", res["Confidence"])
                 save_reaction(key, res["Type"], res["Confidence"])
 
-        return response
 
+        return response
     except Exception as e:
         print(e)
         print("Error processing object {} from bucket {}. ".format(key, bucket) +
               "Make sure your object and bucket exist and your bucket is in the same region as this function.")
         raise e
-
-
